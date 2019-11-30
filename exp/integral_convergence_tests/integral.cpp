b80bd158c95c3020aba1bad1bb6cc86485aa32e6
Adam Robinson <derangedhk417@gmail.com>
2019-11-30 12:39:13 -0500

    Reorganized

---
+++ exp/integral_convergence_tests/integral.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdio>
+
+using namespace std;
+
+int main(int argc, char ** argv) {
+	float A[] = {1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
+	float s[] = {1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
+
+	return 0;
+}
