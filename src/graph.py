fe6a811d9bfeeec0f4b530b21c2ac2890ae3601a
Adam Robinson <derangedhk417@gmail.com>
2019-12-04 07:30:48 -0500

    Made graph look nice

--- src/graph.py
+++ src/graph.py
@@ -21,7 +21,58 @@ if __name__ == '__main__':
 			errors.append(float(b))
 			exps.append(float(c))
 
-	plt.scatter(scales, exps, s=4)
-	plt.xlabel("Scale Factor")
-	plt.ylabel("Ground State Energy Prediction")
+	scales = np.array(scales)
+	errors = np.array(errors)
+	exps   = np.array(exps)
+
+	print("Minimum Energy: %f eV"%(exps.min()))
+	print("a:              %E"%(scales[exps.argmin()]))
+	print("Max Error:      %E eV"%(errors.max()))
+	print("Min Error:      %E eV"%(errors.min()))
+	print("Mean Error:     %E eV"%(errors.mean()))
+
+	scales = np.sqrt(np.log(2) / scales) * 0.2e3
+	mask   = scales < 0.428
+	scales = scales[mask]
+	exps   = exps[mask]
+
+	fig = plt.figure(1)
+	ax  = fig.add_subplot(111)
+
+	buffer_x = (max(scales) - min(scales)) * 0.05
+	buffer_y = (max(exps)   - min(exps))   * 0.05
+
+	_domain = (
+		min(scales) - buffer_x,
+		max(scales) + buffer_x
+	)
+
+	_range = (
+		min(exps) - buffer_y,
+		max(exps) + buffer_y
+	)
+
+	major_ticks_x = np.linspace(
+		_domain[0], _domain[1],
+		10
+	)
+
+	major_ticks_y = np.linspace(
+		_range[0], _range[1],
+		10
+	)
+
+	ax.set_xticks(major_ticks_x)
+	ax.set_yticks(major_ticks_y)
+	ax.grid(which='both', linestyle=':')
+	ax.grid(which='major', alpha=0.9)
+
+	plt.xlim(_domain)
+	plt.ylim(_range)
+
+
+	ax.scatter(scales[::100], exps[::100], s=4)
+	ax.set_xlabel(r"Gaussian Full Width Half Max $[\mathsf{nm}]$")
+	ax.set_ylabel(r"$E_{gs}\;\mathsf{[eV]}$")
+	plt.title("Expectation Value of Energy as a function of Gaussian Full Width Half Max")
 	plt.show()
